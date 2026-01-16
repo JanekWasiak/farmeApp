d801dfe9b109fcf55c55385ca8b741d65ced93d7
JanekWasiak <wasiak_jan@wp.pl>
2026-01-16 15:25:32 +0100

    First commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public String hello = "hello";
+
+public class Item{
+
+    public int wartosc = 0;
+    String nazwa;
+
+}
+
+void main() {
+    int number = 3;
+    String text = "Cześć";
+    char letter = 'A';
+    float num = 2137.6432f;
+    int[] tab = {1,2,3};
+    String[] tab2 = {"String", "hE;;P"};
+    boolean logic = true;
+
+    String[] eq = {"Miecz", "Zbroja" };
+
+    for (int i = 0; i <= eq.length; i++){
+        System.out.println();
+    }
+
+}
+
+
